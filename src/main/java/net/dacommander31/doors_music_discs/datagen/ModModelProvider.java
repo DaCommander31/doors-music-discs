db9ce5e85aed4566d1c723835026433c7c86638d
DaCommander31 <millerjr808@gmail.com>
2025-10-31 19:51:49 +1000

    Update 0.1.0

--- src/main/java/net/dacommander31/doors_music_discs/datagen/ModModelProvider.java
+++ src/main/java/net/dacommander31/doors_music_discs/datagen/ModModelProvider.java
@@ -1,11 +1,11 @@
 package net.dacommander31.doors_music_discs.datagen;
 
 import net.dacommander31.doors_music_discs.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
 import net.minecraft.item.Item;
 
 public class ModModelProvider extends FabricModelProvider {
